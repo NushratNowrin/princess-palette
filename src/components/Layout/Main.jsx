ced0b77ba3d5eea20c8a39fecf7d58e7e2c95578
NushratNowrin <nowrin2023@gmail.com>
2023-05-21 18:59:31 +0600

    footer is done

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Shared/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div className='md:mx-20'>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
