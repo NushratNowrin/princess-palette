e16acdf779694f1c258b3a1cb3b34964e8b3433f
NushratNowrin <nowrin2023@gmail.com>
2023-09-04 05:56:39 +0600

    hero banner modified

--- src/components/Pages/Home/Banner/Banner.jsx
+++ src/components/Pages/Home/Banner/Banner.jsx
@@ -7,8 +7,8 @@ import slider4 from "../../../../assets/slider-4.png";
 const Banner = () => {
 	return (
 		<div>
-			<div className=' carousel w-full h-96 bg-center bg-cover'>
-				<div id='slide1' className='relative carousel-item w-full'>
+			<div className='carousel w-full h-screen bg-center bg-cover pt-28 bg-slate-400'>
+				<div id='slide1' className='relative carousel-item w-full' data-bs-interval="1000">
 					<img src={slider1} className='w-full' />
 					<div className='absolute flex justify-between items-center transform left-0 right-0 top-0 bottom-0 bg-black bg-opacity-60'>
 						<a href='#slide4' className='btn btn-circle'>
@@ -20,7 +20,7 @@ const Banner = () => {
 								for your princess !!!
 							</div>
                             <div className="h-20">
-                            <button className="bg-black text-white font-semibold px-5 py-3 rounded-md mt-5 hover:text-xl ease-in-out duration-75">Buy Now</button>
+                            <button className="bg-violet-900 bg-opacity-60 text-white font-semibold px-5 py-3 rounded-md mt-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-opacity-100 duration-300">Buy Now</button>
                             </div>
 						</div>
 						<a href='#slide2' className='btn btn-circle'>
@@ -28,19 +28,19 @@ const Banner = () => {
 						</a>
 					</div>
 				</div>
-				<div id='slide2' className='carousel-item relative w-full'>
+				<div id='slide2' className='carousel-item relative w-full' data-bs-interval="1000">
 					<img src={slider2} className='w-full' />
 					<div className='absolute flex justify-between items-center transform left-0 right-0 top-0 bottom-0 bg-black bg-opacity-60'>
 						<a href='#slide1' className='btn btn-circle'>
 							❮
 						</a>
 						<div className="text-center">
-							<div className='font-semibold text-white font-serif tracking-wide md:text-3xl text-xl'>
+							<div className='font-bold text-neutral-200 tracking-wide md:text-3xl text-xl'>
 								Buy the best Makeup palette <br />
 								for your princess !!!
 							</div>
                             <div className="h-20">
-                            <button className="bg-neutral-300 text-black font-semibold px-5 py-3 rounded-md mt-5 hover:text-xl ease-in-out duration-75">Buy Now</button>
+                            <button className="bg-violet-900 bg-opacity-60 text-white font-semibold px-5 py-3 rounded-md mt-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-opacity-100 duration-300">Buy Now</button>
                             </div>
 						</div>
 						<a href='#slide3' className='btn btn-circle'>
@@ -48,19 +48,19 @@ const Banner = () => {
 						</a>
 					</div>
 				</div>
-				<div id='slide3' className='carousel-item relative w-full'>
+				<div id='slide3' className='carousel-item relative w-full' data-bs-interval="1000">
 					<img src={slider3} className='w-full' />
 					<div className='absolute flex justify-between items-center transform left-0 right-0 top-0 bottom-0 bg-black bg-opacity-60'>
 						<a href='#slide2' className='btn btn-circle'>
 							❮
 						</a>
-						<div className="text-center ">
-							<div className='font-semibold text-white font-serif tracking-wide md:text-3xl text-xl'>
+						<div className="text-center">
+							<div className='font-bold text-neutral-200 tracking-wide md:text-3xl text-xl'>
 								Buy the best Makeup palette <br />
 								for your princess !!!
 							</div>
                             <div className="h-20">
-                            <button className="bg-white text-black font-semibold px-5 py-3 rounded-md mt-5 hover:text-xl ease-in-out duration-75">Buy Now</button>
+                            <button className="bg-violet-900 bg-opacity-60 text-white font-semibold px-5 py-3 rounded-md mt-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-opacity-100 duration-300">Buy Now</button>
                             </div>
 						</div>
 						<a href='#slide4' className='btn btn-circle'>
@@ -75,12 +75,12 @@ const Banner = () => {
 							❮
 						</a>
 						<div className="text-center">
-							<div className='font-semibold text-white font-serif tracking-wide md:text-3xl text-xl'>
+							<div className='font-bold text-neutral-200 tracking-wide md:text-3xl text-xl'>
 								Buy the best Makeup palette <br />
 								for your princess !!!
 							</div>
                             <div className="h-20">
-                            <button className="bg-black text-white font-semibold px-5 py-3 rounded-md mt-5 hover:text-xl ease-in-out duration-75">Buy Now</button>
+                            <button className="bg-violet-900 bg-opacity-60 text-white font-semibold px-5 py-3 rounded-md mt-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-opacity-100 duration-300">Buy Now</button>
                             </div>
 						</div>
 						<a href='#slide1' className='btn btn-circle'>
