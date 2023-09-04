26749496945f47df6a9d742d054e9f1504c3c32c
NushratNowrin <nowrin2023@gmail.com>
2023-09-04 12:47:16 +0600

    Love section modified

--- src/components/Pages/Home/Love/Love.jsx
+++ src/components/Pages/Home/Love/Love.jsx
@@ -14,7 +14,7 @@ const Love = () => {
         <div className='Lovebg lg:px-32 lg:py-20 md:px-20 py-10 px-5'>
             <div className='md:p-20 p-10 bg-violet-800 bg-opacity-70'>
             <div >
-				<h2 className='md:text-3xl text-xl font-semibold tracking-wide mb-5 text-center text-red-300'>Why Love Us?</h2>
+				<h2 className='md:text-3xl text-xl font-semibold tracking-wide mb-5 text-center text-gray-200'>Why Love Us?</h2>
 				<p className={`${styles.secondaryHeader} text-gray-100`}>
                 We&apos;re a world of imagination, creativity, and endless fun
 				</p>
--- src/components/Pages/Home/Love/SingleLove.jsx
+++ src/components/Pages/Home/Love/SingleLove.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 const SingleLove = (love) => {
-	const { heading, description } = love;
+	const { id, heading, description } = love;
 	return (
-		<div className='rounded-lg  border-2 border-primary p-6 sm:h-80 h-auto serviceBox bg-red-200  mx-auto'>
-			<div className='flex justify-center items-center m-2 '>
+		<div className=' rounded-lg shadow-lg p-6 sm:h-80 h-auto serviceBox bg-gray-200 mt-10  mx-auto'>
+			<div className='flex justify-center items-center m-2 -translate-y-16'>
 				{/* <BiCodeBlock className='text-primary text-5xl p-2 rounded-full border-2 border-primary service-icon' /> */}
+				<div className='text-3xl p-2 h-16 w-16 text-center  rounded-full bg-violet-500 text-white font-fold'>
+					{id}
+				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center">
-            <h1 className='text-xl mb-2 font-semibold'>{heading}</h1>
-			<p className='text-neutral text-[13px] text-center'>{description}</p>
-            </div>
+			<div className='flex flex-col justify-center items-center'>
+				<h1 className='text-xl mb-2 font-semibold'>{heading}</h1>
+				<p className='text-neutral text-[13px] text-center'>{description}</p>
+			</div>
 		</div>
 	);
 };
