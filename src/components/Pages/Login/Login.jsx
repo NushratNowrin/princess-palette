b8baa5a5c852b278d7e23eea674fd3e3b853e27f
NushratNowrin <nowrin2023@gmail.com>
2023-09-04 13:10:34 +0600

    login page padding top done

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -115,7 +115,7 @@ const Login = () => {
 	};
 
 	return (
-		<div className='w-50 p-10 bg-slate-200 text-center'>
+		<div className='w-50 p-10 pt-32 bg-slate-200 text-center'>
 			<div className='email-pass bg-violet-900 md:w-80 m-auto p-16 rounded-md mb-10'>
 				<h4 className='text-2xl font-bold mb-6 text-red-300'>Please Login</h4>
 				<form onSubmit={getLoginData} className='login "text-white'>
