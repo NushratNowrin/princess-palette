521af2ff0a44447043130581c4b5d3337ecaba48
NushratNowrin <nowrin2023@gmail.com>
2023-09-04 07:59:11 +0600

    discount page done

--- src/components/Pages/Home/Discount/Discount.jsx
+++ src/components/Pages/Home/Discount/Discount.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Discount.css";
 import { Link } from "react-router-dom";
+import { styles } from "../../../Style/style";
 
 const Discount = () => {
 	return (
 		<div>
-			<div className='discount flex justify-center items-center my-10 md:px-48'>
-				<div className=' text-center py-12 md:px-20 md:w-11/12 w-80 bg-violet-900 bg-opacity-100 rounded-lg outline-dotted outline-white outline-4 outline-offset-8'>
+			<div className='discount flex justify-center items-center md:px-48'>
+				<div className=' text-center py-12 md:px-20 md:w-11/12 w-80 bg-violet-900 bg-opacity-70 rounded-lg outline-dotted outline-violet-700 outline-4 outline-offset-8'>
 					<h2 className='text-amber-500 text-xl font-bold tracking-wide'>
 						Get 25% Discount
 					</h2>
@@ -15,7 +16,7 @@ const Discount = () => {
 					</p>
 					<div className='h-16 flex items-center justify-center'>
 						<Link to='/all-toys'>
-							<button className='bg-pink-600 text-white px-5 py-3 font-bold rounded-2xl tracking-wide hover:bg-pink-700 hover:text-lg ease-in-out duration-300'>
+							<button className={`${styles.primaryButton} text-white px-5 py-3 font-bold rounded-2xl tracking-wide`}>
 								Show All Items
 							</button>
 						</Link>
